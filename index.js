815257e5a10cc51328c30ce07e8d26e5d35aeb63
1719924451 <1719924451@qq.com>
2020-09-04 16:02:48 +0800

    two

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-// ##配置 git
-// git config --global user.name 'github的用户名'
-// git config --global user.email 'github登录邮箱'
+// ## 配置 git
+// git config --global user.name "github的用户名"
+// git config --global user.email github登录邮箱
+
+// 1. git init: 把文件夹托管给 git
+// 2. git add .: 保存代码
+// 3. git commit -m 'master': 提交代码
+// 4. git remote add origin https://github.com/1937444194/root.git 拷贝自己的这行代码 创建仓库
+// 5. git push -u origin master: 提交代码到 github 
 
 
-/*
-1.	git init
-	// 把文件托管给 git
-2.	git add .
-	// 保存代码
-3.	git commit -m 'master'
-	// 提交代码
-4.	git remote add origin https://github.com/sheng-23/root.git
-	// 创建仓库
-5.	git push -u origin master
-	// 提交
+// 正常进公司以后步骤
+// 1. 创建公钥 私钥
+// ssh-keygen -t rsa -C "你的github邮箱"
+// 2. 打开公钥
+// cat ~/.ssh/id_rsa.pub\
 
-*/
 
-//创建公钥 私钥
+
+// ## 常用命令
+// git status: 查看哪些文件修改了
+// git add 文件名 | .: 保存到本地
+// git commit -m '说明': 提交代码
+// git push origin 远程分支名: 往远程分支提交代码
