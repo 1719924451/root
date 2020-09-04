990c368487840900cc6de56785d2d8ad4b4f3bf9
1719924451 <1719924451@qq.com>
2020-09-04 15:37:57 +0800

    下拉加载效果

---
+++ index.js
@@ -0,0 +1,18 @@
+// ##配置 git
+// git config --global user.name 'github的用户名'
+// git config --global user.email 'github登录邮箱'
+
+
+/*
+1.	git init
+	// 把文件托管给 git
+2.	git add .
+	// 保存代码
+3.	git commit -m 'master'
+	// 提交代码
+4.	git remote add origin https://github.com/sheng-23/root.git
+	// 创建仓库
+5.	git push -u origin master
+	// 提交
+
+*/
